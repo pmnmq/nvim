7cf08ee0b4d00073c3ce36ec8090d08b33d498b4
jichun_pu <jichun_pu@intsig.net>
2024-06-11 18:37:30 +0800

    update

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -13,4 +13,14 @@ return {
     "folke/tokyonight.nvim",
     lazy = true,
   },
+  {
+    "xiyaowong/transparent.nvim",
+    event = "VeryLazy",
+    config = function()
+      local trans = require("transparent")
+      trans.setup()
+      -- trans.clear_prefix('BufferLine')
+      trans.clear_prefix("Neotree")
+    end,
+  },
 }
