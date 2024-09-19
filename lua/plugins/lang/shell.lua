3e758eade1a274e85d91682617fc954807df7895
chun <pujichun@outlook.com>
2024-09-20 00:26:23 +0800

    fix: zsh lsp use bashls

--- lua/plugins/lang/shell.lua
+++ lua/plugins/lang/shell.lua
@@ -2,6 +2,16 @@ local config_formatter = require("plugins.utils.comform").config_formatter
 
 return {
   {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        bashls = {
+          filetypes = { "sh", "zsh", "bash" },
+        },
+      },
+    },
+  },
+  {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       vim.tbl_extend("keep", opts.ensure_installed, { "bash-language-server", "shfmt" })
